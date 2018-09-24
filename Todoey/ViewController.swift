32d8af6ff680363eea2daf355c0c4524dd742a36
Rizwan Aziz <rizwan@rizwans-macbook-pro.local>
2018-09-24 16:00:32 +0500

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Rizwan on 9/24/18.
+//  Copyright © 2018 Rizwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
